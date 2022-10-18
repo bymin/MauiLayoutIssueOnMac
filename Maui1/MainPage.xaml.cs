bea9a4ae05b559c6662b4404feaa520cb2b9bdfb
Ben-ThinkGeo <benbai36@gmail.com>
2022-10-18 09:29:56 -0500

    Init the project

---
+++ Maui1/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿namespace Maui1;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+    void ContentPage_SizeChanged(System.Object sender, System.EventArgs e)
+    {
+        if (CounterBtn.Width <= 0)
+            throw new Exception("Exception2: Component's width -1 in SizeChanged event");
+    }
+
+    void ContentPage_Loaded(System.Object sender, System.EventArgs e)
+    {
+        if (CounterBtn.Width <= 0)
+            throw new Exception("Exception1: Component's width -1 in Loaded event");
+    }
+}
